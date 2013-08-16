da2680c005eda71edc8423dcda89b3edfcc7ff73
spudfkc <potatoz@gmail.com>
2013-08-16 18:01:35 -0400

    Added simple shell

--- minewrap.py
+++ minewrap.py
@@ -1,33 +1,9 @@
 import io
 import os
 
-class Engine:
-  def __init__(self, servers):
-    self.servers = servers
-
-  def startServer(self, server, opts={}):
-    pass
-
-  def stopServer(self, server):
-    pass
-
-  def restartServer(self, server, opts={}):
-    stopServer(server)
-    startServer(server, opts)
-
-class Server:
-  def __init__(self, name, props):
-    self.name = name
-    self.props = props
-
-  def updateProps(self, props):
-  ''' updates server.properties 
-      if no property specified, then no change
-  '''
-    pass
 
 def parseProps(filepath):
-''' parses a properties file into a dict '''
+  ''' parses a properties file into a dict '''
   props = {}
   file = open(filepath)
   lines = file.readlines()
@@ -41,11 +17,11 @@ def parseProps(filepath):
   return props
 
 def loadServers():
-''' loads all the servers from the servers dir 
+  ''' loads all the servers from the servers dir 
     a server.properties file must exist in the 
     server directory for the server to be 
     recognized
-'''
+  '''
   servers = []
   serverDir = 'servers'
   if not os.path.exists(serverDir):
@@ -59,5 +35,60 @@ def loadServers():
   print 'Loaded %d servers' % len(servers)
   return servers
 
-print loadServers()
+class Shell:
+  def __init__(self, engine):
+    self.engine = engine
+    self.cmds = {
+      'start' : engine.startServer,
+      'stop' : engine.stopServer,
+      'restart' : engine.restartServer,
+      'info' : engine.infoServer
+    }
+
+  def getInput(self):
+  ''' Grabs the input from the user, parses it
+      and then returns it '''
+    cmd = raw_input('>')
+    return cmd
+
+  def parseCmd(cmd):
+  ''' parses a string into a list, each element
+      will contain an argument '''
+    # should parse the input string and return a list of args
+    pass
+
+
+class Engine:
+
+  def __init__(self, servers):
+    self.servers = servers
+
+  def startServer(self, server, opts={}):
+    pass
+
+  def stopServer(self, server):
+    pass
+
+  def restartServer(self, server, opts={}):
+    stopServer(server)
+    startServer(server, opts)
+
+  def infoServer(self, server):
+    pass
+
+class Server:
+  started = False
+  def __init__(self, name, props):
+    self.name = name
+    self.props = props
+
+  def updateProp(self, key, value):
+    pass
+
+  def updateProps(self, props):
+    pass
+
+# DO STUFF HERE
+for item in loadServers():
+  print item.name
 
