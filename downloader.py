92a04ce030f61925f36e262a63ce2772ce56534a
spudfkc <potatoz@gmail.com>
2013-08-25 23:22:14 -0400

    added some docs to downloader

--- downloader.py
+++ downloader.py
@@ -11,13 +11,16 @@ class Downloader:
     pass
 
   def download(self, mod, version='default'):
+    ''' download a mod version from the mod registry '''
     url = self.modlinks[mod]
     response = urllib2.urlopen(url)
     self.saveFile(response.read(), os.path.basename(url))
 
-  def load(self):
+  def load(self):sshssh
+    ''' load the mod registry from downloads.json '''
     pass 
 
   def saveFile(self, tosave, where):
+    ''' save the given contents to disk '''
     f = open('tmp/'+where, 'wb')
     f.write(tosave)
